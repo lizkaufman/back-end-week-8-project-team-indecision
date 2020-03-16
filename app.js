64922fca30ab6016e51c218021e1fe4c550c3be0
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-03-16 16:52:34 +0000

    fixed port in pap js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const userRouter = require('./routes/index');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 var corsOptions = {
   origin: 'https://project-week-back-end.herokuapp.com/',
